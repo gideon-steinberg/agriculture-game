4260f0aa543a0f83c266614857877381bb2a6fd6
gideon-steinberg <gideon_steinberg@hotmail.com>
2018-04-30 20:57:14 +1200

    :mouse: Basic input

--- src/Prompt.js
+++ src/Prompt.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 
 
 class Prompt extends Component {
+    constructor() {
+        super();
+        this.state = {
+            action : ""
+        }
+    }
+    handleEnter(event){
+        // the enter key!!
+        if ( event.which === 13 ) {
+            event.preventDefault();
+            var action = this.refs.prompt.value;
+            this.setState({action : "You did " + action});
+            this.refs.prompt.value = "";
+            return false;
+        }
+    }
+    
     render() {
         return (
             <div>
                 What will you do?
                 <br />
+                {this.state.action}
                 <br />
-                <input className="prompt"/>
+                <input
+                    ref="prompt"
+                    className="prompt"
+                    onKeyPress={(event) => this.handleEnter(event)}
+                />
             </div>
         )
     }
